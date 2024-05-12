d382ec62b7036d9fd0372c716ade2308cb8e105a
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-05-12 10:52:42 +0200

    initial commit

---
+++ PlanetarySystem/PlanetarySystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetarySystemApp.swift
+//  PlanetarySystem
+//
+//  Created by Davide Castaldi on 12/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PlanetarySystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
