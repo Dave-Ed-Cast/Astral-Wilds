1872fe89f71f69e82d07444b950642d7a6167839
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-05-12 12:02:50 +0200

    Rotation of planet

--- PlanetarySystem/PlanetarySystemApp.swift
+++ PlanetarySystem/PlanetarySystemApp.swift
@@ -10,7 +10,7 @@ import SwiftUI
 @main
 struct PlanetarySystemApp: App {
     var body: some Scene {
-        WindowGroup {
+        WindowGroup(id: "main"){
             ContentView()
         }
         
--- PlanetarySystem/Planets.swift
+++ PlanetarySystem/Planets.swift
@@ -10,11 +10,30 @@ import RealityKit
 import RealityKitContent
 
 struct Planets: View {
+    
+    @Environment(\.dismissWindow) var dismissWindow
+
     var body: some View {
         RealityView { content in
             if let scene = try? await Entity(named: "Planets", in: realityKitContentBundle) {
                 content.add(scene)
             }
+        }
+        .gesture(TapGesture().targetedToAnyEntity().onEnded({ value in
+            let timer = Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { timer in
+                var transform = value.entity.transform
+                let radians = 90.0 * Float.pi / 180.0
+                transform.rotation *= simd_quatf(angle: radians, axis: SIMD3<Float>(1,0,0))
+                value.entity.move(
+                    to: transform,
+                    relativeTo: nil,
+                    duration: 3,
+                    timingFunction: .linear)
+            }
+            timer.fire()
+        }))
+        .onAppear() {
+            dismissWindow(id: "main")
         }
     }
 }
