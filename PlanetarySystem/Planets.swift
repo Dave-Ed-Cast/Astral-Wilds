781f6e2884ae0ced59ef36ac306cb500728aae9b
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-05-12 17:04:40 +0200

    Added the entirety of the planet system. Next is trying to replicate the motion of the planets around the sun

--- PlanetarySystem/Planets.swift
+++ PlanetarySystem/Planets.swift
@@ -20,17 +20,29 @@ struct Planets: View {
             }
         }
         .gesture(TapGesture().targetedToAnyEntity().onEnded({ value in
-            let timer = Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { timer in
+            
+            var transform = value.entity.transform
+            transform.translation += SIMD3(1, 0, 0)
+            value.entity.move(
+                to: transform,
+                relativeTo: nil,
+                duration: 3,
+                timingFunction: .easeInOut
+            )
+            
+//            let timer = Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { timer in
+            /*
                 var transform = value.entity.transform
                 let radians = 90.0 * Float.pi / 180.0
-                transform.rotation *= simd_quatf(angle: radians, axis: SIMD3<Float>(1,0,0))
+                transform.rotation *= simd_quatf(angle: radians, axis: SIMD3<Float>(2,0,0))
                 value.entity.move(
                     to: transform,
                     relativeTo: nil,
                     duration: 3,
                     timingFunction: .linear)
-            }
-            timer.fire()
+             */
+//            }
+//            timer.fire()
         }))
         .onAppear() {
             dismissWindow(id: "main")
