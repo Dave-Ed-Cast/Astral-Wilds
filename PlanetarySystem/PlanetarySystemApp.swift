bc5311a1273e5f1c4b72370f8008fd5aff2ec560
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-05-12 23:04:34 +0200

    Forgot to comment the app.swift

--- PlanetarySystem/PlanetarySystemApp.swift
+++ PlanetarySystem/PlanetarySystemApp.swift
@@ -10,10 +10,12 @@ import SwiftUI
 @main
 struct PlanetarySystemApp: App {
     var body: some Scene {
+        //to dismiss the view, we declare an id so we can refer to this view
         WindowGroup(id: "main"){
             ContentView()
         }
         
+        //same thing here
         ImmersiveSpace(id: "saturn") {
             Planets()
         }
