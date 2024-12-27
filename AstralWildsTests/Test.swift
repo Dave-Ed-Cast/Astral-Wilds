5f6b1aaa21454ce5a73822a0edb4842b329b916e
Dave-Ed-Cast <148765897+Dave-Ed-Cast@users.noreply.github.com>
2024-12-27 23:14:16 +0100

    Added Tests for Unit testing in future

---
+++ AstralWildsTests/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  AstralWildsTests
+//
+//  Created by Davide Castaldi on 27/12/24.
+//
+
+import Testing
+
+struct Test {
+
+    @Test func test() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
